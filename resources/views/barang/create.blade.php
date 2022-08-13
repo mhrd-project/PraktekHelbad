c2709b19422769dae1dec9464cb5d8f7cbfc9eea
HelBad <helbad3@gmail.com>
2022-08-13 16:36:01 +0700

    dashboard

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.app') @section('title','Tambah Barang')
 @section('home-href')
 {{ url("barang") }}
-@endsection @section('home', 'Barang') @section('breadcrumb','Tambah Barang')
+@endsection @section('home','Barang') @section('breadcrumb','Tambah Barang')
 @section('content')
 <section class="content">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-                <!-- general form elements -->
                 <div class="card card-primary">
                     <div class="card-header">
                         <h3 class="card-title">Tambah Barang</h3>
                     </div>
-                    <!-- /.card-header -->
-                    <!-- form start -->
-                    <form
-                        method="post"
-                        action="{{ url('/barang') }}"
-                    >
-                    @csrf
+                    <form method="post" action="{{ url('/barang') }}">
+                        @csrf
                         <div class="card-body">
                             <div class="form-group">
                                 <label for="nama">Nama Barang <span class="text-danger">*</span></label>
@@ -32,6 +26,7 @@
                                     required
                                 />
                             </div>
+
                             <div class="form-group">
                                 <label for="stok">Stok Barang <span class="text-danger">*</span></label>
                                 <input
@@ -45,6 +40,7 @@
                                     required
                                 />
                             </div>
+                        
                             <div class="form-group">
                                 <label for="harga">Harga <span class="text-danger">*</span></label>
                                 <input
@@ -58,36 +54,30 @@
                                 />
                             </div>
                         </div>
-                        <!-- /.card-body -->
-
+                
                         <div class="card-footer">
                             <div class="btn-group">
                                 <button type="submit" class="btn btn-primary">
                                     Simpan
                                 </button>
-                                <a
-                                    href="{{ url('barang') }}"
-                                    class="btn btn-danger"
-                                    >Batal</a
-                                >
+                                <a href="{{ url('barang') }}" class="btn btn-danger">
+                                    Batal
+                                </a>
                             </div>
                         </div>
                     </form>
                 </div>
-                <!-- /.card -->
             </div>
-            <!-- /.row -->
         </div>
-        <!-- /.container-fluid -->
     </div>
 </section>
+
 @endsection @section('extra_javascript')
 <script>
-    $(document).ready(function (){
+    $(document).ready(function () {
         $('#barang').addClass('active');
     });
-    // Jquery Dependency
-
+    
     $("input[data-type='currency']").on({
         keyup: function () {
             formatCurrency($(this));
@@ -103,51 +93,33 @@
 
     function formatCurrency(input, blur) {
         var input_val = input.val();
-
         if (input_val === "") {
             return;
         }
-
         var original_len = input_val.length;
-
         var caret_pos = input.prop("selectionStart");
 
         if (input_val.indexOf(",") >= 0) {
             var decimal_pos = input_val.indexOf(",");
-
             var left_side = input_val.substring(0, decimal_pos);
             var right_side = input_val.substring(decimal_pos);
-
             left_side = formatNumber(left_side);
-
             right_side = formatNumber(right_side);
-
+            
             if (blur === "blur") {
                 right_side += "00";
             }
-
-            // Limit decimal to only 2 digits
             right_side = right_side.substring(0, 2);
-
-            // join number by .
             input_val = left_side + "," + right_side;
         } else {
-            // no decimal entered
-            // add commas to number
-            // remove all non-digits
             input_val = formatNumber(input_val);
             input_val = input_val;
-
-            // final formatting
             if (blur === "blur") {
                 input_val += ",00";
             }
         }
 
-        // send updated string to input
         input.val(input_val);
-
-        // put caret back in the right position
         var updated_len = input_val.length;
         caret_pos = updated_len - original_len + caret_pos;
         input[0].setSelectionRange(caret_pos, caret_pos);
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,63 +1,48 @@
- <!-- Main Sidebar Container -->
- <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="{{ url('dashboard') }}" class="brand-link">
-      <img src="" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">Praktek</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{asset('assets/AdminLTE/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Admin</a>
-        </div>
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <div class="sidebar">
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="{{asset('image/foto.jpg')}}" class="img-circle elevation-2" alt="User Image">
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a id="dashboard"  href="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <span>Dashboard</span>
-            </a>           
-          </li>
-          <li class="nav-item">
-            <a id="barang" href="/barang" class="nav-link">
-              <i class="nav-icon fas fa-fw fa-folder-open"></i>
-              <span>Barang</span>
-            </a>
-          </li> 
-          <li class="nav-item">
-            <a id="pembelian" href="/pembelian" class="nav-link">
-              <i class="nav-icon fas fa-fw fa-shopping-basket"></i>
-              <span>Pembelian</span>
-            </a>
-          </li> 
-          <li class="nav-item">
-            <a id="penjualan" href="/penjualan" class="nav-link">
-              <i class="nav-icon fas fa-fw fa-shopping-cart"></i>
-              <span>Penjualan</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/logout" class="nav-link">
-              <i class="nav-icon fas fa-fw fa-sign-out-alt"></i>
-              <span>Keluar</span>
-            </a>
-          </li>   
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+      <div class="info">
+        <a href="#" class="d-block">Hilmy</a>
+      </div>
     </div>
-    <!-- /.sidebar -->
-  </aside>
+
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+          <a id="dashboard"  href="/dashboard" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </a>           
+        </li>
+        <li class="nav-item">
+          <a id="barang" href="/barang" class="nav-link">
+            <i class="nav-icon fas fa-fw fa-folder-open"></i>
+            <span>Barang</span>
+          </a>
+        </li> 
+        <li class="nav-item">
+          <a id="pembelian" href="/pembelian" class="nav-link">
+            <i class="nav-icon fas fa-fw fa-shopping-basket"></i>
+            <span>Pembelian</span>
+          </a>
+        </li> 
+        <li class="nav-item">
+          <a id="penjualan" href="/penjualan" class="nav-link">
+            <i class="nav-icon fas fa-fw fa-shopping-cart"></i>
+            <span>Penjualan</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/logout" class="nav-link">
+            <i class="nav-icon fas fa-fw fa-sign-out-alt"></i>
+            <span>Keluar</span>
+          </a>
+        </li>   
+      </ul>
+    </nav>
+  </div>
+</aside>
   
