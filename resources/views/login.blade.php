ab8ca59de4af6d780839a817fb33a281524e7739
HelBad <helbad3@gmail.com>
2022-08-13 13:25:36 +0700

    UI Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,86 +6,28 @@
     <link rel="stylesheet" href="{{ url('login.css') }}">
 </head>
 <body>
+    <div class="form-container">
+        <div class="form-body">
+            <h2 class="title">Login</h2>
+            <form action="{{ url('login') }}" method="POST" class="the-form">
+                @csrf
+                <label for="username">Username</label>
+                <input type="username" name="username" id="username" placeholder="Masukkan Username" required autofocus>
+                @if ($errors->has('username'))
+                <span class="text-danger">{{ $errors->first('username') }}</span>
+                @endif
 
-    <div class="main-container">
-        <div class="form-container">
-            <div class="form-body">
-                <h2 class="title">Login</h2>
-                <form action="" class="the-form">
-
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Enter your email">
-
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Enter your password">
-
-                    <input type="submit" value="Log In">
-
-                </form>
-
-            </div><!-- FORM BODY-->
-
-        </div><!-- FORM CONTAINER -->
-    </div>
-
-</body>
-
-<!-- <body>
-    
-<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand" href="#">Laravel</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Masukkan Password" required>
+                @if ($errors->has('password'))
+                <span class="text-danger">{{ $errors->first('password') }}</span>
+                @endif
+                    
+                <button type="Masuk" class="masuk">
+                    Login
+                </button>
+            </form>
         </div>
     </div>
-</nav>
-  
-<main class="login-form">
-  <div class="cotainer">
-      <div class="row justify-content-center">
-          <div class="col-md-8">
-              <div class="card">
-                  <div class="card-header">Login</div>
-                  <div class="card-body">
-  
-                      <form action="{{ url('login') }}" method="POST">
-                          @csrf
-                          <div class="form-group row">
-                              <label for="email_address" class="col-md-4 col-form-label text-md-right">Username</label>
-                              <div class="col-md-6">
-                                  <input type="text" id="email_address" class="form-control" name="username" required autofocus>
-                                  @if ($errors->has('email'))
-                                      <span class="text-danger">{{ $errors->first('email') }}</span>
-                                  @endif
-                              </div>
-                          </div>
-  
-                          <div class="form-group row">
-                              <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                              <div class="col-md-6">
-                                  <input type="password" id="password" class="form-control" name="password" required>
-                                  @if ($errors->has('password'))
-                                      <span class="text-danger">{{ $errors->first('password') }}</span>
-                                  @endif
-                              </div>
-                          </div>
-  
-                          <div class="col-md-6 offset-md-4">
-                              <button type="submit" class="btn btn-primary">
-                                  Login
-                              </button>
-                          </div>
-                      </form>
-                        
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-</main>
-     
-</body> -->
-
+</body>
 </html>
